8e2b195ed410e701db448c3d73e682d1597d7c13
Sascha Grunert <mail@saschagrunert.de>
2018-06-09 14:53:38 +0200

    Add dom manipulation

--- src/client/login.rs
+++ src/client/login.rs
@@ -3,11 +3,14 @@ use yew::prelude::*;
 use yew::services::console::ConsoleService;
 
 /// Data Model for the Login component
-pub struct LoginComponent {}
+pub struct LoginComponent {
+    name: String,
+}
 
 #[derive(Debug)]
 pub enum Msg {
     Login,
+    UpdateName(String),
 }
 
 impl<C> Component<C> for LoginComponent
@@ -18,12 +21,18 @@ where
     type Properties = ();
 
     fn create(_: (), _: &mut Env<C, Self>) -> Self {
-        LoginComponent {}
+        LoginComponent { name: String::new() }
     }
 
     fn update(&mut self, msg: Self::Message, ctx: &mut Env<C, Self>) -> ShouldRender {
         ctx.as_mut().log(&format!("Message: {:?}", msg));
-        true
+        match msg {
+            Msg::Login => false,
+            Msg::UpdateName(new_name) => {
+                self.name = new_name;
+                true
+            }
+        }
     }
 }
 
@@ -33,7 +42,9 @@ where
 {
     fn view(&self) -> Html<C, Self> {
         html! {
+            <p>{ self.name.chars().rev().collect::<String>() }</p>
             <button onclick=|_| Msg::Login,>{"Login"}</button>
+            <input value=&self.name, oninput=|e| Msg::UpdateName(e.value), />
         }
     }
 }
