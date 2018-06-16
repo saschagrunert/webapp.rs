fed402324c610e71d5fcb451fbeca7da933d922e
Sascha Grunert <mail@saschagrunert.de>
2018-06-16 20:14:35 +0200

    Make protocol service more flexible

--- src/frontend/services/protocol.rs
+++ src/frontend/services/protocol.rs
@@ -1,7 +1,8 @@
 //! A protocol handling service to make (de)serialization comfortable
 
 use capnp::{
-    message::{Builder, HeapAllocator, ReaderOptions},
+    message::{Builder, HeapAllocator, Reader, ReaderOptions},
+    serialize::OwnedSegments,
     serialize_packed::{read_message, write_message},
 };
 use failure::Error;
@@ -14,8 +15,8 @@ pub struct ProtocolService {
 
 #[derive(Debug, Fail)]
 pub enum ProtocolError {
-    #[fail(display = "invalid message type")]
-    InvalidMessageType,
+    #[fail(display = "invalid message type, need: {}", needed_type)]
+    InvalidMessageType { needed_type: String },
 }
 
 impl ProtocolService {
@@ -34,6 +35,11 @@ impl ProtocolService {
         Ok(&self.data)
     }
 
+    /// Read bytes into a reader with owned segments
+    fn read(&mut self, data: &mut [u8]) -> Result<Reader<OwnedSegments>, Error> {
+        Ok(read_message(&mut data.as_ref(), ReaderOptions::new())?)
+    }
+
     /// Create a new login request from a given username and password
     pub fn write_login_request(&mut self, username: &str, password: &str) -> Result<&[u8], Error> {
         {
@@ -49,11 +55,13 @@ impl ProtocolService {
 
     // Get a login response for given bytes
     pub fn read_login_response(&mut self, data: &mut [u8]) -> Result<bool, Error> {
-        let reader = read_message(&mut data.as_ref(), ReaderOptions::new())?;
+        let reader = self.read(data)?;
         let response = reader.get_root::<response::Reader>()?;
         match response.which()? {
             response::Login(data) => Ok(data?.get_success()),
-            _ => Err(Error::from(ProtocolError::InvalidMessageType)),
+            _ => Err(Error::from(ProtocolError::InvalidMessageType {
+                needed_type: "login reseponse".to_owned(),
+            })),
         }
     }
 }
