c816ce88b19e20b3bac1711e85fc90dd3c890fb9
Sascha Grunert <mail@saschagrunert.de>
2018-07-01 15:12:37 +0200

    Add websocket tests

--- src/backend/websocket.rs
+++ src/backend/websocket.rs
@@ -185,3 +185,211 @@ impl WebSocket {
         self.write()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    extern crate futures;
+
+    use self::futures::Stream;
+    use super::*;
+    use actix_web::{test::TestServer, ws};
+
+    #[test]
+    fn succeed_to_login_with_username_and_password() {
+        // Given
+        let mut srv = TestServer::build_with_state(State::default)
+            .start(move |app| app.handler(|req| ws::start(req, WebSocket::new())));
+        let (reader, mut writer) = srv.ws().unwrap();
+
+        // When
+        let mut builder = Builder::new_default();
+        let mut data = Vec::new();
+        {
+            let mut creds = builder.init_root::<request::Builder>().init_login().init_credentials();
+            creds.set_username("username");
+            creds.set_password("username");
+        }
+        write_message(&mut data, &builder).unwrap();
+        writer.binary(data);
+        let (item, _) = srv.execute(reader.into_future()).unwrap();
+
+        // Then
+        match item {
+            Some(Message::Binary(b)) => {
+                let reader = read_message(&mut b.as_ref(), ReaderOptions::new()).unwrap();
+                match reader.get_root::<response::Reader>().unwrap().which().unwrap() {
+                    response::Login(d) => match d.which().unwrap() {
+                        response::login::Token(_) => {}
+                        _ => panic!("Wrong response content"),
+                    },
+                    _ => panic!("Wrong response type"),
+                };
+            }
+            _ => panic!("Wrong message type"),
+        }
+    }
+
+    #[test]
+    fn fail_to_login_with_wrong_username_and_password() {
+        // Given
+        let mut srv = TestServer::build_with_state(State::default)
+            .start(move |app| app.handler(|req| ws::start(req, WebSocket::new())));
+        let (reader, mut writer) = srv.ws().unwrap();
+
+        // When
+        let mut builder = Builder::new_default();
+        let mut data = Vec::new();
+        {
+            let mut creds = builder.init_root::<request::Builder>().init_login().init_credentials();
+            creds.set_username("username");
+            creds.set_password("password");
+        }
+        write_message(&mut data, &builder).unwrap();
+        writer.binary(data);
+        let (item, _) = srv.execute(reader.into_future()).unwrap();
+
+        // Then
+        match item {
+            Some(Message::Binary(b)) => {
+                let reader = read_message(&mut b.as_ref(), ReaderOptions::new()).unwrap();
+                match reader.get_root::<response::Reader>().unwrap().which().unwrap() {
+                    response::Login(d) => match d.which().unwrap() {
+                        response::login::Error(_) => {}
+                        _ => panic!("Wrong response content"),
+                    },
+                    _ => panic!("Wrong response type"),
+                };
+            }
+            _ => panic!("Wrong message type"),
+        }
+    }
+
+    #[test]
+    fn succeed_to_login_with_token() {
+        // Given
+        let mut srv = TestServer::build_with_state(State::default)
+            .start(move |app| app.handler(|req| ws::start(req, WebSocket::new())));
+        let (reader, mut writer) = srv.ws().unwrap();
+
+        // When
+        let mut builder = Builder::new_default();
+        let mut data = Vec::new();
+        {
+            let mut creds = builder.init_root::<request::Builder>().init_login().init_credentials();
+            creds.set_username("username");
+            creds.set_password("username");
+        }
+        write_message(&mut data, &builder).unwrap();
+        writer.binary(data);
+        let (item, _) = srv.execute(reader.into_future()).unwrap();
+        let token = match item {
+            Some(Message::Binary(b)) => {
+                let reader = read_message(&mut b.as_ref(), ReaderOptions::new()).unwrap();
+                match reader.get_root::<response::Reader>().unwrap().which().unwrap() {
+                    response::Login(d) => match d.which().unwrap() {
+                        response::login::Token(token) => token.unwrap().to_owned(),
+                        _ => panic!("Wrong response content"),
+                    },
+                    _ => panic!("Wrong response type"),
+                }
+            }
+            _ => panic!("Wrong message type"),
+        };
+        let mut token_data = Vec::new();
+        builder.init_root::<request::Builder>().init_login().set_token(&token);
+        write_message(&mut token_data, &builder).unwrap();
+        let (reader, mut writer) = srv.ws().unwrap();
+        writer.binary(token_data);
+        let (token_item, _) = srv.execute(reader.into_future()).unwrap();
+
+        // Then
+        match token_item {
+            Some(Message::Binary(b)) => {
+                let reader = read_message(&mut b.as_ref(), ReaderOptions::new()).unwrap();
+                match reader.get_root::<response::Reader>().unwrap().which().unwrap() {
+                    response::Login(d) => match d.which().unwrap() {
+                        response::login::Token(_) => {}
+                        _ => panic!("Wrong response content"),
+                    },
+                    _ => panic!("Wrong response type"),
+                }
+            }
+            _ => panic!("Wrong message type"),
+        }
+    }
+
+    #[test]
+    fn fail_to_login_with_wrong_token() {
+        // Given
+        let mut srv = TestServer::build_with_state(State::default)
+            .start(move |app| app.handler(|req| ws::start(req, WebSocket::new())));
+        let (reader, mut writer) = srv.ws().unwrap();
+
+        // When
+        let mut builder = Builder::new_default();
+        let mut data = Vec::new();
+        let token = "wrong_token".to_owned();
+        builder.init_root::<request::Builder>().init_login().set_token(&token);
+        write_message(&mut data, &builder).unwrap();
+        writer.binary(data);
+        let (item, _) = srv.execute(reader.into_future()).unwrap();
+
+        // Then
+        match item {
+            Some(Message::Binary(b)) => {
+                let reader = read_message(&mut b.as_ref(), ReaderOptions::new()).unwrap();
+                match reader.get_root::<response::Reader>().unwrap().which().unwrap() {
+                    response::Login(d) => match d.which().unwrap() {
+                        response::login::Error(_) => {}
+                        _ => panic!("Wrong response content"),
+                    },
+                    _ => panic!("Wrong response type"),
+                };
+            }
+            _ => panic!("Wrong message type"),
+        }
+    }
+
+    #[test]
+    fn succeed_to_logout() {
+        // Given
+        let mut srv = TestServer::build_with_state(State::default)
+            .start(move |app| app.handler(|req| ws::start(req, WebSocket::new())));
+        let (reader, mut writer) = srv.ws().unwrap();
+
+        // When
+        let mut builder = Builder::new_default();
+        let mut data = Vec::new();
+        let token = "token".to_owned();
+        builder.init_root::<request::Builder>().set_logout(&token);
+        write_message(&mut data, &builder).unwrap();
+        writer.binary(data);
+        let (item, _) = srv.execute(reader.into_future()).unwrap();
+
+        // Then
+        match item {
+            Some(Message::Binary(b)) => {
+                let reader = read_message(&mut b.as_ref(), ReaderOptions::new()).unwrap();
+                match reader.get_root::<response::Reader>().unwrap().which().unwrap() {
+                    response::Logout(_) => {}
+                    _ => panic!("Wrong response type"),
+                };
+            }
+            _ => panic!("Wrong message type"),
+        }
+    }
+
+    #[test]
+    fn succeed_to_close_the_connection() {
+        // Given
+        let mut srv = TestServer::build_with_state(State::default)
+            .start(move |app| app.handler(|req| ws::start(req, WebSocket::new())));
+        let (reader, mut writer) = srv.ws().unwrap();
+
+        // When
+        writer.close(None);
+
+        // Then
+        assert!(srv.execute(reader.into_future()).is_err());
+    }
+}
