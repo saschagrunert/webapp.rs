8b38855924e8372a77842a2bceccbd8fb8f47903
Sascha Grunert <mail@saschagrunert.de>
2019-01-09 20:14:54 +0100

    Update lib.rs

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,28 +1,5 @@
 //! The backend library
-
 #![deny(missing_docs)]
-
-extern crate actix;
-extern crate actix_web;
-extern crate bytes;
-extern crate diesel;
-#[macro_use]
-extern crate failure;
-extern crate futures;
-extern crate jsonwebtoken;
-#[macro_use]
-extern crate log;
-extern crate num_cpus;
-extern crate openssl;
-extern crate r2d2;
-extern crate serde;
-extern crate serde_cbor;
-#[macro_use]
-extern crate serde_derive;
-extern crate time;
-extern crate url;
-extern crate uuid;
-extern crate webapp;
 
 mod cbor;
 mod database;
