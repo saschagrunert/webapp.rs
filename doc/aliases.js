879662e49a13c711cd8761656ee014d0b0f9e276
CircleCI <mail@saschagrunert.de>
2019-01-13 17:26:05 +0000

    Update documentation

--- doc/aliases.js
+++ doc/aliases.js
@@ -1,11 +1,11 @@
 var ALIASES = {};
-ALIASES['app'] = {};
+ALIASES["app"] = {};
+ALIASES["backend"] = {};
+ALIASES["webapp"] = {};
+ALIASES["webapp_backend"] = {};
+ALIASES["webapp_frontend"] = {};
 ALIASES['app'] = {};
 ALIASES['backend'] = {};
-ALIASES['backend'] = {};
-ALIASES['webapp'] = {};
 ALIASES['webapp'] = {};
 ALIASES['webapp_backend'] = {};
-ALIASES['webapp_backend'] = {};
-ALIASES['webapp_frontend'] = {};
 ALIASES['webapp_frontend'] = {};
