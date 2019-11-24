6199989b4074534abe31cdc984bc37a96fb7945a
CircleCI <mail@saschagrunert.de>
2019-11-24 05:45:22 +0000

    Update documentation

--- doc/implementors/yew/html/trait.Renderable.js
+++
@@ -1,9 +0,0 @@
-(function() {var implementors = {};
-implementors["webapp_frontend"] = [{text:"impl Renderable&lt;<a class=\"struct\" href=\"webapp_frontend/struct.RootComponent.html\" title=\"struct webapp_frontend::RootComponent\">RootComponent</a>&gt; for <a class=\"struct\" href=\"webapp_frontend/struct.RootComponent.html\" title=\"struct webapp_frontend::RootComponent\">RootComponent</a>",synthetic:false,types:["webapp_frontend::component::root::RootComponent"]},];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
