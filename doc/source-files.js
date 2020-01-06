80c45d3d79ad2bd01b5f4c1bcc03af63ea162920
CircleCI <mail@saschagrunert.de>
2020-01-06 16:29:20 +0000

    Update documentation

--- doc/source-files.js
+++ doc/source-files.js
@@ -2,6 +2,6 @@ var N = null;var sourcesIndex = {};
 sourcesIndex["app"] = {"name":"","files":["main.rs"]};
 sourcesIndex["backend"] = {"name":"","files":["main.rs"]};
 sourcesIndex["webapp"] = {"name":"","dirs":[{"name":"protocol","files":["mod.rs","model.rs","request.rs","response.rs"]}],"files":["config.rs","lib.rs","schema.rs"]};
-sourcesIndex["webapp_backend"] = {"name":"","dirs":[{"name":"http","files":["login_credentials.rs","login_session.rs","logout.rs","mod.rs"]},{"name":"server","files":["mod.rs"]},{"name":"token","files":["mod.rs"]}],"files":["cbor.rs","database.rs","lib.rs"]};
+sourcesIndex["webapp_backend"] = {"name":"","dirs":[{"name":"http","files":["login_credentials.rs","login_session.rs","logout.rs","mod.rs"]},{"name":"server","files":["mod.rs"]},{"name":"token","files":["mod.rs"]}],"files":["database.rs","lib.rs"]};
 sourcesIndex["webapp_frontend"] = {"name":"","dirs":[{"name":"component","files":["content.rs","login.rs","mod.rs","root.rs"]},{"name":"service","files":["cookie.rs","log.rs","mod.rs","session_timer.rs","uikit.rs"]}],"files":["api.rs","lib.rs","route.rs","string.rs"]};
 createSourceSidebar();
