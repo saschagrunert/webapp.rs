798953b69cef2bf01194087c0337265e1b78bd88
saschagrunert <saschagrunert@users.noreply.github.com>
2026-09-17 05:48:26 +0000

    deploy: 56d5fabcb2a46d923b0013913ca75e61e6bbef39

---
+++ src-files.js
@@ -0,0 +1,2 @@
+createSrcSidebar('[["webapp",["",[["pages",[],["content.rs","login.rs","mod.rs"]]],["app.rs","auth.rs","csrf.rs","database.rs","lib.rs","rate_limit.rs"]]]]');
+//{"start":19,"fragment_lengths":[135]}
